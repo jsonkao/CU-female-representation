20f7930e169f106752ee4dea07c2f2626b163ff2
Jason Kao <jasonkao85@gmail.com>
2018-10-20 17:12:58 -0400

    graphs

--- src/main.js
+++ src/main.js
@@ -1,26 +1,94 @@
-const pipeJSON = '{"Humanities": {"Tenured Faculty": [[39, 40, 43, 45, 51, 53, 55, 55, 55, 57], [80, 80, 78, 79, 86, 89, 83, 86, 88, 90]], "Graduate Students": [[443, 433, 410, 420, 388, 361, 352, 356, 325, 307], [371, 355, 337, 327, 333, 322, 332, 333, 311, 321]], "Tenure Eligible Faculty": [[31, 34, 35, 37, 34, 28, 27, 23, 20, 19], [33, 33, 33, 30, 27, 24, 24, 24, 22, 21]], "Undergraduate Major and Concentrator Students": [[480, 513, 461, 412, 433, 444, 465, 462, 421, 387], [359, 351, 341, 308, 314, 289, 314, 349, 340, 257]]}, "Natural Sciences": {"Tenured Faculty": [[16, 15, 16, 17, 19, 19, 20, 21, 24, 30], [115, 116, 119, 119, 117, 120, 121, 123, 123, 125]], "Graduate Students": [[245, 259, 273, 272, 272, 256, 255, 250, 253, 252], [386, 368, 349, 353, 340, 358, 371, 367, 369, 366]], "Tenure Eligible Faculty": [[7, 9, 11, 13, 14, 16, 16, 17, 11, 9], [27, 25, 29, 27, 27, 24, 24, 26, 27, 31]], "Undergraduate Major and Concentrator Students": [[287, 298, 294, 329, 372, 368, 384, 450, 505, 424], [259, 257, 290, 303, 314, 324, 329, 378, 415, 450]]}, "Social Sciences": {"Tenured Faculty": [[22, 24, 26, 27, 30, 31, 32, 30, 30, 31], [79, 83, 83, 87, 90, 90, 88, 90, 90, 90]], "Graduate Students": [[296, 292, 282, 269, 242, 228, 222, 229, 240, 241], [352, 340, 322, 324, 304, 387, 288, 300, 310, 303]], "Tenure Eligible Faculty": [[13, 16, 20, 24, 25, 26, 24, 25, 24, 20], [26, 25, 24, 17, 19, 18, 18, 19, 20, 21]], "Undergraduate Major and Concentrator Students": [[459, 476, 486, 514, 533, 522, 533, 503, 456, 456], [598, 591, 582, 615, 636, 596, 608, 605, 595, 605]]}, "Arts and Sciences": {"Tenured Faculty": [[77, 79, 85, 89, 100, 103, 107, 106, 109, 118], [274, 279, 280, 285, 293, 299, 292, 299, 301, 305]], "Graduate Students": [[984, 984, 965, 961, 902, 845, 829, 835, 818, 800], [1109, 1063, 1008, 1004, 977, 1067, 991, 1000, 990, 990]], "Tenure Eligible Faculty": [[51, 59, 66, 74, 73, 70, 67, 65, 55, 48], [86, 83, 86, 74, 73, 66, 66, 69, 69, 73]], "Undergraduate Major and Concentrator Students": [[1226, 1287, 1241, 1255, 1338, 1334, 1382, 1415, 1382, 1267], [1216, 1199, 1213, 1226, 1264, 1209, 1251, 1332, 1350, 1312]]}}';
-const pipeCounts = JSON.parse(pipeJSON);
+const START_YEAR = 2004;
+const END_YEAR = 2013;
+const YEARS = d3.range(START_YEAR, END_YEAR + 1);
+
+const divisions = [
+  'Arts and Sciences', 
+  'Humanities', 
+  'Natural Sciences', 
+  'Social Sciences'
+];
+const ranks = [
+  'Undergraduate Major and Concentrator Students',
+  'Graduate Students',
+  'Tenure Eligible Faculty',
+  'Tenured Faculty',
+];
+let data;
+
+const getPercents = (divIdx, rankIdx) => {
+  if (divIdx >= divisions.length || rankIdx >= ranks.length) {
+    throw new RangeError('Division or rank does not exist.');
+  }
+  div = divisions[divIdx];
+  rank = ranks[rankIdx];
+  const [women, men] = data[div][rank];
+
+  return women.map((w, i) => w / (w + men[i]));
+};
+
+const width = 600,
+  height = 400;
+
+const drawChart = (div, rank) => {
+  const margin = {top: 50, right: 50, bottom: 50, left: 50};
+  const gWidth = width - margin.left - margin.right;
+  const gHeight = height - margin.top - margin.bottom;
+
+  const percents = getPercents(div, rank);
+
+  const xScale = d3.scaleLinear()
+    .domain([START_YEAR, END_YEAR])
+    .range([0, gWidth]);
+  const yScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([gHeight, 0]);
+
+  const fillBelow = d3.area()
+    .x((_, i) => xScale(START_YEAR + i))
+    .y0(gHeight)
+    .y1(yScale)
+  const fillAbove = d3.area()
+    .x((_, i) => xScale(START_YEAR + i))
+    .y0(yScale)
+    .y1(0);
+
+  const svg = d3.select('div#chart-container')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${gHeight})`)
+    .call(d3.axisBottom(xScale).tickFormat(d3.format('')));
+
+  svg.append('g')
+    .attr('class', 'y axis')
+    .call(d3.axisLeft(yScale).tickFormat(d3.format('.0%')));
+
+  svg.append('path')
+    .datum(percents)
+    .attr('class', 'area below')
+    .attr('d', fillBelow);
+  svg.append('path')
+    .datum(percents)
+    .attr('class', 'area above')
+    .attr('d', fillAbove);
+
+};
+
+d3.json('data/pipe_counts.json')
+  .then(json => {
+    data = json;
+    drawChart(0, 0);
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
 const width = 600;
 const height = 300;
 const numPoints = 10;
@@ -67,3 +135,5 @@ capture.on('mousedown', () => {
       capture.on('mousemove', null).on('mouseup', null);
     });
 });
+*/
+
