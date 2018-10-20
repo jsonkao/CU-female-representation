9316208c7033b9ac0ecd4aedcf8207b88d560cfe
Jason Kao <jasonkao85@gmail.com>
2018-10-20 15:05:06 -0400

    yattle

--- src/main.js
+++ src/main.js
@@ -1,45 +1,46 @@
 const width = 600;
 const height = 300;
 const numPoints = 10;
-const svg = d3.select('svg')
+const svg = d3
+  .select('svg')
   .attr('width', width)
   .attr('height', height);
 
-const background = svg.append('rect')
-  .attr('class', 'background')
-  .attr('width', width)
-  .attr('height', height);
+const bands = svg
+  .append('g')
+  .selectAll('rect.band')
+  .data(new Array(numPoints));
 
-const bands = svg.append('g');
-
-bands.selectAll('rect.band')
-  .data(new Array(numPoints))
+bands
   .enter()
   .append('rect')
   .attr('height', height)
   .attr('width', width / numPoints)
-  .attr('class','band')
+  .attr('class', 'band')
   .attr('x', (_, i) => width * i / numPoints);
 
-const line = d3.line()
-  .x(d => d[0])
-  .y(d => d[1]);
+// when time for 2 fillBt's, check out https://d3indepth.com/shapes/, stack.offset()
+const fillBt = d3.area().y0(height);
 
 const pathdata = {};
-const path = svg.append('path')
-  .attr('class', 'yourpath');
+const path = svg.append('path').attr('class', 'yourpath');
 
-background
-  .on('mousedown',() => {
-    background
-      .on('mousemove', function(d, i) {
-        position = Math.round(d3.event.offsetX / (width / numPoints));
-        pathdata[position] = [position * width / numPoints, d3.mouse(this)[1]];
-        path.datum(Object.values(pathdata)).attr('d', line);
-      })
-      .on('mouseup',() => {
-        background
-          .on('mousemove', null)
-          .on('mouseup', null);
-      });
-  });
+const capture = svg
+  .append('rect')
+  .attr('class', 'background')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('fill', 'none')
+  .attr('pointer-events', 'all');
+
+capture.on('mousedown', () => {
+  capture
+    .on('mousemove', function(d, i) {
+      position = Math.round(d3.event.offsetX / (width / numPoints));
+      pathdata[position] = [position * width / numPoints, d3.mouse(this)[1]];
+      path.datum(Object.values(pathdata)).attr('d', fillBt);
+    })
+    .on('mouseup', () => {
+      capture.on('mousemove', null).on('mouseup', null);
+    });
+});
